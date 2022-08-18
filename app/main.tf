fc519ae7ff1de2adc1cce26d6bc2f9a47bb8b7dd
Bruno Pinheiro <brunopinheiro@IT-NMC-BRUPINH1.local>
2022-08-18 10:48:00 -0300

    Commit

--- app/main.tf
+++ app/main.tf
@@ -3,5 +3,5 @@ module "ec2" {
   instance_count = var.instance_count
   ami            = var.ami
   instance_type  = var.instance_type
-  private_ip     = var.private_ip
+  #private_ip     = var.private_ip
 }
--- app/variables.tf
+++ app/variables.tf
@@ -1,4 +1,4 @@
 variable "instance_count" {}
 variable "ami" {}
 variable "instance_type" {}
-variable "private_ip" {}
+#variable "private_ip" {}
