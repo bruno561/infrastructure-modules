3cd38bac6dc10098483ce82a59b818f999fceb32
Bruno Pinheiro <brunopinheiro@IT-NMC-BRUPINH1.local>
2022-08-18 10:26:40 -0300

    Second commit

--- app/main.tf
+++ app/main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-  # ... other provider settings ...
-}
-terraform {
-  backend "s3" {}
-}
 module "ec2" {
   source         = "../modules/ec2"
   instance_count = var.instance_count
